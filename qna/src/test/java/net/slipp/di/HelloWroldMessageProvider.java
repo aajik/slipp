9425ef170cdc9858816e36fd6b7c2d3d263bcdcd
aajik <aajik@PC054.paygate.net>
2016-11-18 19:10:50 +0900

    ad

---
+++ qna/src/test/java/net/slipp/di/HelloWroldMessageProvider.java
@@ -0,0 +1,11 @@
+package net.slipp.di;
+
+public class HelloWroldMessageProvider implements MessageProvider {
+
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return "Hello World!";
+	}
+
+}
